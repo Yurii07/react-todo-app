99cf352622f7056cdb471d0268bdc20dfec0884f
Yurii Adamenko <skyey07@gmail.com>
2019-04-10 17:11:43 +0300

    first commit

---
+++ src/components/item-add-form/item-add-form.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+import './item-add-form.css';
+
+export default class ItemAddForm extends Component{
+
+    state = {
+        label: ''
+    };
+
+    onLabelChange = (event) => {
+        this.setState({
+           label: event.target.value
+        });
+    };
+
+    onSubmit = (event) => {
+        event.preventDefault();
+        this.props.onItemAdded(this.state.label);
+        this.setState({
+            label: ''
+        });
+    };
+
+    render () {
+        return (
+            <form className = "item-add-form d-flex"
+                  onSubmit={this.onSubmit}
+            >
+                <input type="text"
+                       className="form-control new-todo-label"
+                       value={this.state.label}
+                       onChange={this.onLabelChange}
+                       placeholder="What needs to be done?" />
+
+                <button type="submit"
+                        className="btn btn-outline-secondary">Add</button>
+            </form>
+        )
+    }
+}
